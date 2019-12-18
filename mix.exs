4d3ff4989b7dc659a4d0b13eddca4139b272abaf
Michael Vanasse <michael@simplecast.com>
2019-12-17 17:49:43 -0700

    remove hex properties

--- mix.exs
+++ mix.exs
@@ -2,28 +2,24 @@ defmodule Concerto.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :concerto,
-     description: "file-based routing library for elixir",
-     version: "0.1.5",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     package: package()]
+    [
+      app: :concerto,
+      description: "file-based routing library for elixir",
+      version: "0.1.5",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger]
+    ]
   end
 
   defp deps do
     []
-  end
-
-  defp package do
-    [files: ["lib", "mix.exs", "README*"],
-     maintainers: ["Cameron Bytheway"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/exstruct/concerto"}]
   end
 end
